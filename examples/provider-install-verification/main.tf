5afea8e9ed364f1d8d540fb424495c15418cd93f
pixam <github@pixelizedambitions.com>
2025-07-02 20:39:23 +1000

    Add provider install verification example
    Introduces a new Terraform example to verify installation of the 'podman' provider. This includes provider configuration and a sample data resource.

---
+++ examples/provider-install-verification/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    podman = {
+      source = "registry.terraform.io/pixambi/podman"
+    }
+  }
+}
+
+provider "podman" {}
+
+data "podman_example" "example" {}
